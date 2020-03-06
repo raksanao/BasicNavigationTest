8dd986e7286acebb2077e3323659be8c5f1f5cc2
Rukhshona Olimova <rukhshona87@gmail.com>
2020-03-06 13:35:13 -0600

    HomeWork 1

--- src/test/java/com/cbt/utilities/TitleVerification3.java
+++ src/test/java/com/cbt/utilities/TitleVerification3.java
@@ -7,11 +7,14 @@ import java.util.List;
 
 public class TitleVerification3 {
     public static void main(String[] args) {
-        List<String> urls = Arrays.asList("https://lulugandgeorgia.com",
+//        List<String> urls = Arrays.asList("https://lulugandgeorgia.com",
+//                "https://wayfair.com/",
+//                "https://walmart.com ", "https://westelm.com/");
+
+        List<String> urls = Arrays.asList("https://www.luluandgeorgia.com/",
                 "https://wayfair.com/",
-                "https://walmart.com ", "https://westelm.com/");
-
-
+                "https://walmart.com",
+                "https://westelm.com/");
         WebDriver driver = BrowserFactory.getDriver("chrome");
 
         for (String eachUrl : urls) {
